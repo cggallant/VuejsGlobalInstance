83e4cc4544e2a4ccc961ed2978aaaf2c6fc89da3
cggallant <c.gerard.gallant@gmail.com>
2020-09-08 10:08:01 -0300

    Adjusted to indicate that the code has been moved under the blog_post_code repository.

--- VuejsGlobalInstance/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-Vue.config.productionTip = true;
-Vue.prototype.$myModule = null; // Will hold the module's instance
-
-new Vue({
-    render: h => h(App)
-}).$mount('#app');
